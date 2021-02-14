48844b9ae4f1a3eb0b097943c4013eb948572cf8
YYI4403 <YYI4403@192.168.1.1>
2021-02-14 11:35:03 +0100

    moda

--- src/main/java/demo/HelloWorld.java
+++ src/main/java/demo/HelloWorld.java
@@ -3,8 +3,11 @@ package demo;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("ciao");
+		
+		for(Integer i=0; i < 10; i++) {
+			System.out.println("ciao "+ i);			
+		}
+
 	}
 
 }
